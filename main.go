0539326eb0e384a676932795878e56db3d9dc94b
Raihanpoke <raihanmalay14@gmail.com>
2023-08-15 10:01:30 +0700

    just for learn

--- main.go
+++ main.go
@@ -3,14 +3,15 @@ package main
 import (
 	"log"
 	"net/http"
+
 )
 
 func main() {
-	router := http.NewServeMux()
 
 	// middleware loggger
-	router.Handle("/", middleware.logger(http.HandlerFunc(controllers.index)))
+	// router.Handle("/", middleware.logger(http.HandlerFunc(controllers.index)))
 
+	http.HandleFunc("/", controllers.Index)
 	port := "8000"
 	log.Println("server berjalan pada http://localhost:%\n", port)
 	http.ListenAndServe(port, nil)
